366ef2aaa9eec1b611e49507c0a71c54d4755ac9
João Teixeira <joao@indielayer.com>
2022-03-03 15:30:20 +0000

    docs(progress): progress numbers instead of strings

--- packages/docs/src/pages/component/progress/-snippets/usage.vue
+++ packages/docs/src/pages/component/progress/-snippets/usage.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="grid grid-cols-3 gap-4">
-    <x-progress percentage="10" color="amber"/>
-    <x-progress percentage="50" color="primary"/>
-    <x-progress percentage="100" color="success"/>
+    <x-progress :percentage="10" color="amber"/>
+    <x-progress :percentage="50" color="primary"/>
+    <x-progress :percentage="100" color="success"/>
   </div>
 </template>
