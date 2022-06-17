6892af66d2637fb96f2e3d25a2db3fd10925d7e7
João Teixeira <joao@indielayer.com>
2022-06-17 11:43:09 +0100

    fix(create-ui): icons path

--- packages/create-ui/templates/vue/code/default/src/components/WelcomeItem.vue
+++ packages/create-ui/templates/vue/code/default/src/components/WelcomeItem.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="mt-10 flex">
     <div class="mt-1">
-    <slot name="icon"></slot>
+      <slot name="icon"></slot>
     </div>
     <div class="flex-1 ml-4">
       <h3 class="text-xl font-semibold">
--- packages/create-ui/templates/vue/code/router/src/App.vue
+++ packages/create-ui/templates/vue/code/router/src/App.vue
@@ -6,17 +6,17 @@ import HelloWorld from '@/components/HelloWorld.vue'
 <template>
   <div class="flex items-center justify-center h-screen bg-[#001e26] dark text-[#accac8] flex-col">
     <div class="max-w-2xl">
-    <header>
-      <div class="wrapper">
-        <HelloWorld msg="Welcome!" />
+      <header>
+        <div class="wrapper">
+          <HelloWorld msg="Welcome!" />
 
-        <nav class="space-x-4 my-4">
-          <x-link to="/" color="amber" underline>Home</x-link>
-          <x-link to="/about" color="amber" underline>About</x-link>
-        </nav>
-      </div>
-    </header>
-    <RouterView />
+          <nav class="space-x-4 my-4">
+            <x-link to="/" color="amber" underline>Home</x-link>
+            <x-link to="/about" color="amber" underline>About</x-link>
+          </nav>
+        </div>
+      </header>
+      <RouterView />
     </div>
   </div>
 </template>
