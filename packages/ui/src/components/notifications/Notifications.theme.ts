3c7a22b38ecb730ba0dc4f8ffa2e9f9d9bcd9dcb
João Teixeira <joao@indielayer.com>
2022-09-05 22:31:14 +0100

    style(notifications): add border for notification item

--- packages/ui/src/components/notifications/Notifications.theme.ts
+++ packages/ui/src/components/notifications/Notifications.theme.ts
@@ -6,6 +6,6 @@ export default {
 
     list: 'flex flex-col items-end w-full sm:w-[520px] px-4',
 
-    item: 'w-full flex items-center rounded-md px-4 py-3 bg-gray-800 dark:bg-gray-50 text-white dark:text-gray-900',
+    item: 'w-full flex items-center rounded-md px-4 py-3 bg-gray-800 dark:bg-gray-50 text-white dark:text-gray-900 border border-gray-700 dark:border-gray-100',
   },
 }
