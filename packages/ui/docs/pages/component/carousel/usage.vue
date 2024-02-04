c3be9fc1dc4aa410c4ce0c11c372d9300856445b
João Teixeira <joao@indielayer.com>
2024-02-04 15:18:26 +0000

    docs: export documentation (#36)

--- packages/ui/docs/pages/component/carousel/usage.vue
+++ packages/ui/docs/pages/component/carousel/usage.vue
@@ -1,10 +1,10 @@
 <template>
   <x-carousel class="!h-80 rounded-md" class-dots="!justify-start pl-10">
     <x-carousel-slide>
-      <img src="@/assets/images/photo2.jpeg" alt="" />
+      <img src="../../../assets/images/photo2.jpeg" alt="" />
     </x-carousel-slide>
     <x-carousel-slide>
-      <img src="@/assets/images/photo1.jpeg" alt="" />
+      <img src="../../../assets/images/photo1.jpeg" alt="" />
     </x-carousel-slide>
   </x-carousel>
 </template>
