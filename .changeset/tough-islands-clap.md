512afe35b86bbcfeecd6bf25ec339ca78a2a1c80
João Teixeira <joao@indielayer.com>
2025-02-25 16:27:28 +0000

    chore(form): expose validate and submit methods

---
+++ .changeset/tough-islands-clap.md
@@ -0,0 +1,5 @@
+---
+"@indielayer/ui": patch
+---
+
+chore(form): expose validate and submit methods
--- packages/ui/src/components/form/Form.vue
+++ packages/ui/src/components/form/Form.vue
@@ -132,6 +132,8 @@ const submit = (e: Event) => {
   emit('submit', isFormValid)
 }
 
+defineExpose({ validate, submit })
+
 const { styles, classes, className } = useTheme('Form', {}, props)
 </script>
 
