f03cdec23d91c8aeeaf794136e59b1f6eb93c995
João Teixeira <joao@indielayer.com>
2025-10-07 08:42:08 +0100

    feat(table): add link prop

--- packages/ui/src/components/table/theme/TableCell.base.theme.ts
+++ packages/ui/src/components/table/theme/TableCell.base.theme.ts
@@ -7,17 +7,26 @@ const theme: TableCellTheme = {
 
       c.push(props.dense ? 'h-9 py-0.5' : 'h-11 py-1')
 
-      if (props.textAlign === 'left') c.push('text-left')
-      else if (props.textAlign === 'center') c.push('text-center')
-      else if (props.textAlign === 'right') c.push('text-right')
-      else if (props.textAlign === 'justify') c.push('text-justify')
+      if (props.to || props.href) {
+        c.push('flex items-center')
 
-      if (props.verticalAlign === 'baseline') c.push('align-baseline')
-      else if (props.verticalAlign === 'bottom') c.push('align-bottom')
-      else if (props.verticalAlign === 'middle') c.push('align-middle')
-      else if (props.verticalAlign === 'text-bottom') c.push('align-text-bottom')
-      else if (props.verticalAlign === 'text-top') c.push('align-text-top')
-      else if (props.verticalAlign === 'top') c.push('align-top')
+        if (props.textAlign === 'left') c.push('justify-start')
+        else if (props.textAlign === 'center') c.push('justify-center')
+        else if (props.textAlign === 'right') c.push('justify-end')
+        else if (props.textAlign === 'justify') c.push('justify-center')
+      } else {
+        if (props.textAlign === 'left') c.push('text-left')
+        else if (props.textAlign === 'center') c.push('text-center')
+        else if (props.textAlign === 'right') c.push('text-right')
+        else if (props.textAlign === 'justify') c.push('text-justify')
+
+        if (props.verticalAlign === 'baseline') c.push('align-baseline')
+        else if (props.verticalAlign === 'bottom') c.push('align-bottom')
+        else if (props.verticalAlign === 'middle') c.push('align-middle')
+        else if (props.verticalAlign === 'text-bottom') c.push('align-text-bottom')
+        else if (props.verticalAlign === 'text-top') c.push('align-text-top')
+        else if (props.verticalAlign === 'top') c.push('align-top')
+      }
 
       if (props.truncate) c.push('truncate')
 
