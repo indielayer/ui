1a995f80fe1eeb5244f322688b441f41e8a8d346
João Teixeira <joao@indielayer.com>
2026-05-20 23:15:08 +0100

    chore: update to pnpm 11

--- packages/ui/src/components/divider/theme/Divider.base.theme.ts
+++ packages/ui/src/components/divider/theme/Divider.base.theme.ts
@@ -4,7 +4,7 @@ const theme: DividerTheme = {
   classes: {
     wrapper: ({ props }) => `flex justify-center items-center ${props.vertical ? 'h-full flex-col' : 'w-full'}`,
 
-    label: 'font-medium text-sm text-secondary-600 dark:text-secondary-300',
+    label: 'font-medium text-xs text-slate-400 dark:text-secondary-300',
 
     line: 'bg-secondary-200 dark:bg-secondary-700 flex-grow',
   },
